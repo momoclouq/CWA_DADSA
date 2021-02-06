6f489fed355a288063f6058472b2b3b2190e6ccc
momocloud <phamhoangminh050899@gmail.com>
2021-02-06 19:06:03 +0700

    first commit

---
+++ Node.py
@@ -0,0 +1,5 @@
+class Node:
+    def __init__(self, data):
+        self.item = data
+        self.next = None  # reference to the next node
+        self.pre = None  # reference to the previous node
--- Store.py
+++ Store.py
@@ -1,6 +1,46 @@
 class Store:
+    #Constructors
     def __init__(self, item, name):
+        #items will not be duplicated, this is enforced by the user
         self.item = []
         self.name = name
-       
-        #self.print = print("Item list at {1} is: {0} . ".format(item, name))
+    
+    #method for usage
+    def checkInStore(item):
+        #check if item is in the store
+        #use binary search, based on the fact that the items are sorted
+        return binary_search(0, self.item.len(), item)
+
+    def sortStore():
+        #sort the items in the store for faster searching (binary search)
+        return;
+
+    
+    ############################
+    #algorithm methods
+
+    def binary_search(self, low, high, x):
+    #Recursive case
+        if (high >= low):
+            mid = (high + low) // 2
+    
+            #If element is at the middle -> found the value
+            if self.item[mid] == x:
+                return True
+            #If element is smaller than the middle value -> search in the lower half
+            elif self.item[mid] > x:
+                return binary_search(arr, low, mid - 1, x)
+            #If element is larger than the middle value -> search in the upper half
+            else:
+                return binary_search(arr, mid + 1, high, x)
+            
+        else:
+            # Element is not present in the array
+            return False
+
+
+    def quick_sort():
+        return;
+
+    
+    
